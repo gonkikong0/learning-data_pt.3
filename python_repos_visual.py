51873b0e415148d60de5d9b8822041d4ce0f7a65
gonkikong0 <toshivy@outlook.com>
2025-10-29 13:40:36 +0530

    Adding tooltips

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -14,12 +14,16 @@ response_dict = r.json()
 #Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 
 
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
 
 #Making the Visualization #NOTE: Need(s) - Data to plot, Layout to form
 
@@ -27,6 +31,12 @@ data = [{
     'type': 'bar',
     'x': repo_names,
     'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line' : {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
 }]
 
 my_layout = {
